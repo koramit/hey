7f290b63f1a8522c9e90a2df02a3453d2447b24b
Koramit Pichanaharee <koramit.p@gmail.com>
2020-11-05 20:12:49 +0700

    add missing use

--- app/Http/Controllers/Services/TelegramWebhooksController.php
+++ app/Http/Controllers/Services/TelegramWebhooksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Services;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request;
 
 class TelegramWebhooksController extends Controller
