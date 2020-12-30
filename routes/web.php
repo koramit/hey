3ace26980918c89603c93ef7ea2f86195b731ad7
Koramit Pichanaharee <koramit.p@gmail.com>
2020-12-30 14:23:23 +0700

    add uptime

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\Services\LINEWebhooksController;
 use App\Http\Controllers\Services\TelegramWebhooksController;
+use App\Models\MonitorService;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,23 +23,22 @@ Route::get('/', function () {
     return 'Hey ! v0.1';
 });
 
-
 // WEBKOOKS
 Route::post('/webhooks/line', LINEWebhooksController::class);
 Route::post('/webhooks/telegram/{token}', TelegramWebhooksController::class);
 
 // MONITOR
 Route::post('/monitor', function () {
-    $request = \Request::all();
+    $request = Request::all();
     if (($request['token'] ?? null) !== env('MONITOR_TOKEN')) {
         abort(401);
     }
 
     if ($request['errors'] !== false) {
-        \Log::error(json_encode($request['errors']));
+        Log::error(json_encode($request['errors']));
     }
 
-    $services = \Cache::get('services', ['valve' => [], 'ad' => [], 'scabbers' => []]);
+    $services = Cache::get('services', ['valve' => [], 'ad' => [], 'scabbers' => []]);
 
     foreach (['valve', 'ad', 'scabbers', 'smuggle'] as $service) {
         $services[$service][] = [
@@ -44,9 +47,24 @@ Route::post('/monitor', function () {
             'error' => $request['data'][$service]['error'] ?? null,
         ];
     }
-    \Cache::put('services', $services);
+    Cache::put('services', $services);
 });
 
 Route::get('/monitor', function () {
-    return view('monitor', ['services' => \Cache::get('services', [])]) ;
+    return view('monitor', ['services' => Cache::get('services', [])]);
+});
+
+Route::post('uptimes', function () {
+    $data = Request::all();
+
+    $service = MonitorService::whereName($data['name'] ?? null)->first();
+
+    if (! $service) {
+        return;
+    }
+
+    $service->uptimes()->create([
+        'online' => $data['online'],
+        'timestamp' => now(),
+    ]);
 });
