fcee3123e73430ac98bfe866275f864d06b7d402
Koramit Pichanaharee <koramit.p@gmail.com>
2021-01-10 11:31:43 +0700

    remove monitor

--- app/Managers/WordpleaseMonitorManager.php
+++ app/Managers/WordpleaseMonitorManager.php
@@ -10,11 +10,8 @@ class WordpleaseMonitorManager extends MonitorManager
     public function handleDowntime()
     {
         $boot = Http::withOptions(['verify' => false])->get(env('WORDPLEASE_BOOT_URL'));
-        Log::info($boot->body());
         if (! $boot->ok()) {
             Log::error('WORDPLEASE BOOT URL IS DOWN');
-        } else {
-            Log::error('WORDPLEASE BOOT URL IS UP');
         }
         parent::handleDowntime();
     }
